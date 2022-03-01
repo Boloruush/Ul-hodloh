280e8c574c4e67549db1386a465f773218b674b8
Boloruush <bolor.a0803@gmail.com>
2022-03-01 08:17:21 +0800

    grey container

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: ListView(
-          children: const [
-            ListTile(
-              title: Text('Ul hodloh'),
-              subtitle: Text('15,373 zar'),
-              leading: Icon(Icons.home),
-            ),
-            ListTile(
-              title: Text('Ul hodloh'),
-              subtitle: Text('15,373 zar'),
-              leading: Icon(Icons.home),
-            ),
-            ListTile(
-              title: Text('Ul hodloh'),
-              subtitle: Text('15,373 zar'),
-              leading: Icon(Icons.home),
-            ),
-          ],
-        ),
-      ),
+    return Container(
+      color: Colors.grey,
     );
   }
 }
